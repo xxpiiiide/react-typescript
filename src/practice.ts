be35f08e3c6ae82261db348085bf70b50cb1a960
이 필 제 <xxpiiiide@gmail.com>
2021-06-14 05:38:49 +0900

    배열 내장함수 타입

--- src/practice.ts
+++ src/practice.ts
@@ -6,3 +6,10 @@ sum(1,2)
 sum('q', 'n'); // 함수안에서도 타입을 지정하여 줄 수 있다.(number로 지정했으므로 string값은 받지 못한다.)
 
 
+function sumArray(numbers: number[]): number {
+    return numbers.reduce((acc, current) => acc+current, 0)
+}
+
+const total = sumArray([1,2,3,4,5])
+console.log(total)
+
