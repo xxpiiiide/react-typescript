28c203677fcf24e28acc9a3a618ffe9b067e5432
이 필 제 <xxpiiiide@gmail.com>
2021-06-14 05:30:47 +0900

    tsc명령어입력

--- dist/practice.js
+++ dist/practice.js
@@ -1,7 +1,21 @@
-'use strict';
-var message = 'hello world';
-console.log(message);
-
+"use strict";
+// const message: string = 'hello world';
+// console.log(message)
 // meessage는 string이므로 number인 1은 될 수 없다고 뜬다.
 // const message: string = 1;
 // console.log(MessagePort)
+
+
+var count = 0; //숫자
+count += 1;
+count = "갑자기 분위기 문자열"; // 현재 에러 발생
+var message = 'hello world'; // 문자열
+var done = true; // 불리언 값
+var numbers = [1, 2, 3]; // 숫자배열
+var messages = ['hello', 'wolrd']; // 문자배열
+messages.push(1); // 이거는 문자배열안에 숫자를 넣으려고 하기 때문에 되지 않을 것이다.
+var mightBeUndefined = undefined; // mightBeUndefined가 string일 수도 있고, undefined일 수도 있다.
+var nullableNumber = null; // nullableNumber가 number일 수도 있고, null일 수도 있다.
+var color = 'red'; // 'red'나 'orange'나 'yellow'중에 'red'가 하나임
+color = 'yellow';
+color = 'green'; // 현재 에러 발생
