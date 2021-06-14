6001a5e70c5ab0985fe4b41815dfe425205b6b5d
이 필 제 <xxpiiiide@gmail.com>
2021-06-14 10:01:24 +0900

    일반 객체를 interface로 타입설정하기

--- dist/practice.js
+++ dist/practice.js
@@ -1,44 +1,12 @@
 "use strict";
-// 클래스에서 interface를 implements하기
-var Circle = /** @class */ (function () {
-    // `implements` 키워드를 사용하여 해당 클래스가 shape interface의 조건을 충족하겠다는 것을 명시하여야 한다.
-    // radius: number; // 멤버 변수 radius 값을 설정한다.
-    //  constructor(radius: number) {
-    //     this.radius = radius;
-    // }
-    // 타입스크립트에서는 constructor의 파라미터 쪽에 public 또는 private accessor를 사용하면 직접 하나하나 설정해주는 작업을 생략할 수 있다.
-    function Circle(radius) {
-        this.radius = radius;
-        this.radius = radius;
-    }
-    // 너비를 가져오는 함수를 구현한다.
-    Circle.prototype.getArea = function () {
-        return this.radius * this.radius * Math.PI; // Math.PI속성은 원주와 지름비율인 약 3.14를 나타낸다.
-    };
-    return Circle;
-}());
-var Rectangle = /** @class */ (function () {
-    // width: number;
-    // height: number;
-    // constructor(width: number, height: number) {
-    //     this.width = width;
-    //     this.height = height ;
-    // }
-    // 위 constructor에서 width, height선언 후 멤버 변수를 하나하나 설정해주었는데, 타입스크립트에서는 constructor의 파라미터 쪽에 public 또는 private accessor를 사용하면 직접 하나하나 설정해주는 작업을 생략할 수 있다.
-    function Rectangle(width, height) {
-        this.width = width;
-        this.height = height;
-        this.width = width;
-        this.height = height;
-    }
-    Rectangle.prototype.getArea = function () {
-        return this.width * this.height;
-    };
-    return Rectangle;
-}());
-var circle = new Circle(5);
-var rectangle = new Rectangle(10, 5);
-console.log(circle.radius); // public로 선언된 값이므로 클래스 외부에서 조회가 가능하다.
-// console.log(rectangle.width);    // private로 선언된 값은 클래스 외부에서 조회가 불가능하다.
-var shpaes = [new Circle(5), new Rectangle(10, 5)];
-shpaes.forEach(function (shape) { return console.log(shape.getArea()); }); // forEach()메서드는 각 배열 요소에 대해 제공된 함수를 한 번씩 실행한다.
+// 일반 객체를 interface로 타입 설정하기
+var person = {
+    name: '이필제',
+    age: 31
+};
+var expert = {
+    name: '허이나',
+    age: 1,
+    skills: ['javascript', 'react']
+};
+var people = [person, expert];
--- src/practice.ts
+++ src/practice.ts
@@ -1,61 +1,38 @@
-// 클래스에서 interface를 implements하기
+// 일반 객체를 interface로 타입 설정하기
 
 
-// shape 이라는 interface를 선언한다.
-interface Shape {
-    getArea(): number; // Shape interface에는 getArea라는 함수가 꼭 있어야 하며 해당함수는 반환값은 숫자이다.
+// interface Person {
+//     name: string;
+//     age?: number;   // 여기서 물음표는 설정을 해도 되고 안해도 되는 값을 뜻한다.
+
+// }
+
+// interface Developer {
+//     name: string;
+//     age?: number;
+//     skills: string[]
+// }
+// Person과 Developer가 형태가 유사할 경우 interface 선언 시 다른 interface를 extends 키워드를 사용해서 상속받을 수 있다.
+
+interface Person {
+    name: string;
+    age?: number;
 }
 
-
-class Circle implements Shape {
-    // `implements` 키워드를 사용하여 해당 클래스가 shape interface의 조건을 충족하겠다는 것을 명시하여야 한다.
-    
-    // radius: number; // 멤버 변수 radius 값을 설정한다.
-    //  constructor(radius: number) {
-    //     this.radius = radius;
-    // }
-    // 타입스크립트에서는 constructor의 파라미터 쪽에 public 또는 private accessor를 사용하면 직접 하나하나 설정해주는 작업을 생략할 수 있다.
-
-    constructor(public radius: number) { // public으로 선언된 값은 클래스 외부에서 조회할 수 있다. 
-        this.radius = radius;
-    }
-   
-
-
-    // 너비를 가져오는 함수를 구현한다.
-    getArea() {
-        return this.radius * this.radius * Math.PI; // Math.PI속성은 원주와 지름비율인 약 3.14를 나타낸다.
-    }
+interface Developer extends Person {
+    skills: string[]
 }
 
-class Rectangle implements Shape {
-    
-    // width: number;
-    // height: number;
-
-    // constructor(width: number, height: number) {
-    //     this.width = width;
-    //     this.height = height ;
-    // }
-    // 위 constructor에서 width, height선언 후 멤버 변수를 하나하나 설정해주었는데, 타입스크립트에서는 constructor의 파라미터 쪽에 public 또는 private accessor를 사용하면 직접 하나하나 설정해주는 작업을 생략할 수 있다.
-
-    constructor(private width: number, private height: number) { //private로 선언된 값은 클래스 내부에서만 조회할 수 있다.
-        this.width = width;
-        this.height =  height;
-    }
-
-    getArea() { 
-        return this.width * this.height;
-    }
+const person: Person = {
+    name: '이필제',
+    age: 31
 }
 
+const expert: Developer = {
+    name: '허이나',
+    age: 1,
+    skills: ['javascript', 'react']
+}
 
-const circle = new Circle(5)
-const rectangle = new Rectangle(10, 5)
+const people: Person[] = [person, expert]
 
-console.log(circle.radius);     // public로 선언된 값이므로 클래스 외부에서 조회가 가능하다.
-// console.log(rectangle.width);    // private로 선언된 값은 클래스 외부에서 조회가 불가능하다.
-
-const shpaes: Shape[] = [new Circle(5), new Rectangle(10, 5)]
-
-shpaes.forEach( shape => console.log(shape.getArea())) // forEach()메서드는 각 배열 요소에 대해 제공된 함수를 한 번씩 실행한다.
