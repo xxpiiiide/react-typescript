2b5d88036e615b5dd5a8ecf147686ea0590ead48
이 필 제 <xxpiiiide@gmail.com>
2021-06-14 10:48:02 +0900

    함수에서 Generics 사용

--- dist/practice.js
+++ dist/practice.js
@@ -1,12 +1,32 @@
 "use strict";
-// Type Alias 사용하기
-var person = {
-    name: '이필제'
+// Generics
+// 함수에서 Generics 사용하기
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
-var expert = {
-    name: '허이나',
-    skills: ['javascript', 'react']
-};
-var people = [person, expert];
-var color = 'red';
-var colors = ['red', 'orange'];
+// function merge(a: any, b: any): any {
+//     return {
+//         ...a,
+//         ...b
+//     }
+// }
+// 위 merge함수를 Generics 이용하기
+function merge(a, b) {
+    return __assign(__assign({}, a), b);
+}
+var merged = merge({ foo: 1 }, { bar: 1 });
+// Generics 이용한 또다른 함수 예
+function wrap(param) {
+    return {
+        param: param
+    };
+}
+var wrapped = wrap(10);
--- src/practice.ts
+++ src/practice.ts
@@ -1,29 +1,34 @@
-// Type Alias 사용하기
+// Generics
+// 함수에서 Generics 사용하기
 
-type Person = {
-    name: string
-    age?: number 
+// function merge(a: any, b: any): any {
+//     return {
+//         ...a,
+//         ...b
+//     }
+// }
+
+// 위 merge함수를 Generics 이용하기
+function merge<A, B>(a: A, b: B): A & B {
+    return {
+        ...a, 
+        ...b
+    }
 }
 
+const merged = merge({foo: 1}, {bar: 1})
 
-// &는 Intersection으로 2개 이상의 타입들을 합쳐준다.
-type Developer = Person & {
-    skills: string[];
+
+// Generics 이용한 또다른 함수 예
+function wrap<T>(param: T) {
+    return { 
+        param
+    }
 }
 
-const person: Person = {
-    name: '이필제'
-}
-
-const expert: Developer = {
-    name: '허이나',
-    skills: ['javascript', 'react']
-}
+const wrapped = wrap(10)
 
 
-type People = Person[]
-const people: People = [person, expert]
 
-type Color = 'red' | 'orange' | 'yellow';
-const color: Color = 'red'
-const colors: Color[] = ['red', 'orange']
+
+
